8aeb61ac6d5c4277ce80ec9aa0053d589aea6631
Nam Nguyen Tran <nn36@rice.edu>
2026-06-21 23:45:11 -0500

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        close_to_op = {
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+        for i in s:
+            if i not in close_to_op:
+                stack.append(i)
+            elif i in close_to_op and len(stack) != 0 and stack[-1] != close_to_op[i] :
+                return False
+            elif i in close_to_op and len(stack) != 0 and stack[-1] == close_to_op[i]:
+                stack.pop()
+            else:
+                return False
+        return stack == []
